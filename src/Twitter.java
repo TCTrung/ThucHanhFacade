79249df74cf7214efd3eb8c650483007c5e13121
TCTrung <trungtrinh1325@gmail.com>
2023-11-07 15:04:02 +0700

    hi

---
+++ src/Twitter.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Twitter implements SocialShare{
+    private String message;
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void share() {
+        System.out.println("Sharing to Twitter: " + this.message);
+    }
+}
+
+
+
+
